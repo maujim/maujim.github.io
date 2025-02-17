abf8c444a7766d6f1647e664615068f495630ae2
Mukund Mauji <mauji.mukund@yahoo.ca>
2025-02-16 23:37:09 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { AsciiRenderer } from '@react-three/drei';
+
+import './App.css';
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta;
+  });
+
+  const scale = 1;
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 * scale : scale}
+      onClick={event => click(!clicked)}
+      onPointerOver={event => hover(true)}
+      onPointerOut={event => hover(false)}
+    >
+      <torusKnotGeometry />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+}
+
+function App() {
+  return (
+    <Canvas>
+      <AsciiRenderer />
+
+      <color attach="background" args={['black']} />
+
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+
+      <Box position={[0, 0, 0]} />
+    </Canvas>
+  );
+}
+
+export default App;
